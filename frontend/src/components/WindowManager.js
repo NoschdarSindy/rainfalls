cecf4700e2074400514863572004deb8e2f0d6b2
Max Paul Patzelt <max.paul.patzelt@student.hu-berlin.de>
2023-01-16 11:34:38 +0100

    Fix time stamp values in GlobalTimeline

--- frontend/src/components/WindowManager.js
+++ frontend/src/components/WindowManager.js
@@ -1,65 +1,47 @@
 const INSETS = {
   all: [
-      "0 75% 0% 0%",
-      "0% 0% 0% 25%",
-      "0% 37.5% 0% 25%",
-      "0% 0% 0% 62.5%",
-      "0% 0% 0% 62.5%"
+    "0 75% 0% 0%",
+    "0% 0% 0% 25%",
+    "0% 37.5% 0% 25%",
+    "0% 0% 0% 62.5%",
+    "0% 0% 0% 62.5%",
   ],
   intervals: [
-      "0% 100% 0% 0%",
-      "0%",
-      "0% 50% 0% 0%",
-      "0% 0% 0% 50%",
-      "0% 0% 0% 50%"
+    "0% 100% 0% 0%",
+    "0%",
+    "0% 50% 0% 0%",
+    "0% 0% 0% 50%",
+    "0% 0% 0% 50%",
   ],
   comparisonIntA: [
-      "0% 50% 0% 0%",
-      "0% 0% 0% 50%",
-      "0% 0% 0% 50%",
-      "0% 0% 0% 100%",
-      "0% 0% 0% 100%"
+    "0% 50% 0% 0%",
+    "0% 0% 0% 50%",
+    "0% 0% 0% 50%",
+    "0% 0% 0% 100%",
+    "0% 0% 0% 100%",
   ],
   comparisonIntB: [
-      "0% 50% 0% 0%",
-      "0% 0% 0% 50%",
-      "0% 50% 0% 50%",
-      "0% 0% 0% 50%",
-      "0% 0% 0% 50%"
+    "0% 50% 0% 0%",
+    "0% 0% 0% 50%",
+    "0% 50% 0% 50%",
+    "0% 0% 0% 50%",
+    "0% 0% 0% 50%",
   ],
   comparison: [
-      "0%",
-      "0% 0% 0% 100%",
-      "0% 0% 0% 100%",
-      "0% 0% 0% 100%",
-      "0% 0% 0% 100%"
+    "0%",
+    "0% 0% 0% 100%",
+    "0% 0% 0% 100%",
+    "0% 0% 0% 100%",
+    "0% 0% 0% 100%",
   ],
-  intervalA: [
-      "0% 100% 0% 0%",
-      "0%",
-      "0%",
-      "0% 0% 0% 100%",
-      "0% 0% 0% 100%"
-  ],
-  intervalB: [
-      "0% 100% 0% 0%",
-      "0%",
-      "0% 100% 0% 0%",
-      "0%",
-      "0%"
-  ],
-  none: [
-    "100%",
-    "100%",
-    "100%",
-    "100%",
-    "100%"
-  ]
-}
+  intervalA: ["0% 100% 0% 0%", "0%", "0%", "0% 0% 0% 100%", "0% 0% 0% 100%"],
+  intervalB: ["0% 100% 0% 0%", "0%", "0% 100% 0% 0%", "0%", "0%"],
+  none: ["100%", "100%", "100%", "100%", "100%"],
+};
 
 export default class WindowManager {
   static isVisible(id) {
-    let element = document.getElementById(`${id}-mosaic`)
+    let element = document.getElementById(`${id}-mosaic`);
     if (!element) {
       return false;
     }
@@ -67,48 +49,45 @@ export default class WindowManager {
     let parentStyle = element.parentElement.style;
     let inset = parseInt(parentStyle.inset);
     let right = parseInt(parentStyle.right);
-    let left =  parseInt(parentStyle.left);
-  
+    let left = parseInt(parentStyle.left);
+
     return inset !== 100 && left + right !== 100;
-  } 
+  }
 
   static setInset(id) {
-    const root = document.getElementsByClassName("mosaic")[0]
-    console.log(root);
+    const root = document.getElementsByClassName("mosaic")[0];
     if (!root) {
       return;
     }
 
     let element = root.firstChild.firstChild;
     if (element.nodeName === "#text") {
-      element = element.parentNode.nextSibling.firstChild
+      element = element.parentNode.nextSibling.firstChild;
     }
 
     let i = 0;
     do {
       element.style.inset = INSETS[id][i];
       i += 1;
-    }
-    while (element = element.nextSibling);
+    } while ((element = element.nextSibling));
 
-    const message = document.getElementById('noWindowPresentMessage');
+    const message = document.getElementById("noWindowPresentMessage");
 
     if (id === "none") {
       if (!message) {
         let frame = root.firstChild;
         let child = frame.firstChild;
-        let node = document.createElement('p');
-        node.id = 'noWindowPresentMessage'
-        node.innerHTML = "No Window present - Please use the view dropdown to select the wanted windows."
+        let node = document.createElement("p");
+        node.id = "noWindowPresentMessage";
+        node.innerHTML =
+          "No Window present - Please use the view dropdown to select the wanted windows.";
         node.style.padding = "5px";
         frame.insertBefore(node, child);
+      } else {
+        message.style.display = "inline-block";
       }
-      else {
-        message.style.display = "inline-block"
-      }
-    }
-    else if (id !== "none" && message) {
-      message.style.display = "none"
+    } else if (id !== "none" && message) {
+      message.style.display = "none";
     }
   }
 
@@ -116,10 +95,10 @@ export default class WindowManager {
     if (selected.length === 3) {
       return WindowManager.setInset("all");
     }
-    
+
     if (selected.length === 2) {
-      if (selected.some(obj => obj.value === "comparison")) {
-        if (selected.some(obj => obj.value === "intervalA")) {
+      if (selected.some((obj) => obj.value === "comparison")) {
+        if (selected.some((obj) => obj.value === "intervalA")) {
           return WindowManager.setInset("comparisonIntA");
         }
         return WindowManager.setInset("comparisonIntB");
@@ -164,7 +143,7 @@ export default class WindowManager {
         if (id === "intervalA") {
           return WindowManager.setInset("comparison");
         }
-        return
+        return;
       }
 
       if (intervalB) {
@@ -174,13 +153,13 @@ export default class WindowManager {
         if (id === "intervalB") {
           return WindowManager.setInset("comparison");
         }
-        return
+        return;
       }
 
       if (id === "comparison") {
         return WindowManager.setInset("none");
       }
-      return
+      return;
     }
 
     if (intervalA) {
@@ -192,13 +171,13 @@ export default class WindowManager {
           return WindowManager.setInset("intervalA");
         }
       }
-      
+
       if (id === "intervalA") {
         return WindowManager.setInset("none");
       }
     }
 
-    if (intervalB) {    
+    if (intervalB) {
       if (id === "intervalB") {
         return WindowManager.setInset("none");
       }
